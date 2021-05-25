703201bafaad265167b0307cef03f046e0353148
mashym91 <76610676+mashym91@users.noreply.github.com>
2021-05-25 20:33:09 +0900

    曜日関連の冗長的な部分を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -24,27 +24,24 @@ today = Date.today
 # 引数指定がなければ今日の月、年を設定
 month = options['m']&.to_i || today.month
 year = options['y']&.to_i || today.year
-
 beginning_of_month = Date.new(year, month, 1) # 月初
 end_of_month = Date.new(year, month, -1) # 月末
-wdays = %w[日 月 火 水 木 金 土]
-beginning_of_month_wday = wdays[beginning_of_month.wday] # 月初の曜日
-wday_index = wdays.index(beginning_of_month_wday) # 曜日index
 
 # print部分
 print "      #{month}月 #{year}
-#{wdays.join(' ')}\n"
+#{%w[日 月 火 水 木 金 土].join(' ')}\n"
 
 (beginning_of_month.day..end_of_month.day).each do |num|
+  wday = Date.new(year, month, num).wday # 曜日取得
+
   # 1日より前の未設定部分を空白表示
-  print ('   ' * wday_index).to_s if num == 1
+  print ('   ' * wday).to_s if num == 1
 
   display_date = num.to_s.rjust(2) # 右詰め表示
   # 今日の日付部分を反転
   display_date = "\e[47m\e[30m#{display_date}\e[0m" if month == today.month && year == today.year && num == today.day
   print "#{display_date} " # 余白調整（スペースを1つ入れる）
 
-  # 土曜で改行
-  print "\n" if Date.new(year, month, num).wday == 6
+  print "\n" if wday == 6 # 土曜で改行
 end
 print "\n"

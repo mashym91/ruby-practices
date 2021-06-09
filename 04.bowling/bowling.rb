e34a6d122c7139e8c6453b778a5567f64a3d4017
mashym91 <76610676+mashym91@users.noreply.github.com>
2021-06-09 10:34:37 +0900

    scoreをeachでまわして処理するように変更
    フレーム毎の配列だと次のフレームのスコアが絡んでくると扱いにくいため

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,33 +1,46 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score_string = ARGV[0]
+scores = ARGV[0].split(',')
+sum = 0
+is_jump = false
+frame_count = 1
 
-shots = []
-shots_per_game = []
-count = 1
-
-score_split = score_string.split(',')
-score_split.each_with_index do |score, index|
-  shots_per_game << score
-
-  if score == 'X' || count == 2
-    shots << shots_per_game
-    shots_per_game = []
-    count = 1
+def calc_score(score)
+  if score == 'X'
+    10
   else
-    count += 1
+    score.to_i
   end
-
-  next unless shots.count == 10
-
-  if score == 'X' # 10 frame is strike
-    shots.last << score_split[index + 1]
-    shots.last << score_split[index + 2]
-  elsif score_split[index - 1].to_i + score.to_i == 10  # spare
-    shots.last << score_split[index + 1]
-  end
-  break
 end
 
-# calc score
+scores.each_with_index do |score, index|
+  if is_jump
+    is_jump = false
+    next
+  end
+
+  if frame_count == 10
+    sum += calc_score(score)
+    sum += calc_score(scores[index + 1])
+    sum += calc_score(scores[index + 2]) if scores[index + 2]
+    break
+  elsif score == 'X' # strike
+    sum += 10
+    # add next 2shot
+    sum += calc_score(scores[index + 1])
+    sum += calc_score(scores[index + 2])
+  elsif score.to_i + (scores[index + 1]).to_i == 10 # spare
+    sum += 10
+    sum += calc_score(scores[index + 2]) # add next 1shot
+    is_jump = true
+  else
+    # add 2shot
+    sum += score.to_i + (scores[index + 1]).to_i
+    is_jump = true
+  end
+
+  frame_count += 1
+end
+
+puts sum

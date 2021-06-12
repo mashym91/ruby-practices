44afd4e169fd9c49a34b3bab3cd373dfe063be34
mashym91 <76610676+mashym91@users.noreply.github.com>
2021-06-12 09:37:35 +0900

    lオプションのテストケース追加

--- 05.ls/test/ls_command_test.rb
+++ 05.ls/test/ls_command_test.rb
@@ -35,14 +35,6 @@ class LsCommandTest < Minitest::Test
     assert_equal result, ls.exec
   end
 
-  # def test_exec_with_l_options
-  #   ls = LsCommand.new(['l'], TEST_DIR)
-  #   result <<~TEXT
-
-  #   TEXT
-  #   assert_equal result, ls.exec
-  # end
-
   def test_exec_with_r_options
     ls = LsCommand.new(['r'], TEST_DIR)
     result = <<~TEXT
@@ -54,11 +46,43 @@ class LsCommandTest < Minitest::Test
     assert_equal result, ls.exec
   end
 
-  #def test_exec_with_many_options
-  #  ls = LsCommand.new(['alr'], TEST_DIR)
-  #  result <<~TEXT
+  def test_exec_with_l_options
+    ls = LsCommand.new(['l'], TEST_DIR)
+    result = <<~TEXT
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 a.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 b.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 c.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 d.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 e.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 f.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 g.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 h.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 i.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 j.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 k.txt
+    TEXT
+    # ヒアドキュメントで最後に改行コードが入るので削除
+    assert_equal result.gsub(/\n$/, ''), ls.exec
+  end
 
-  #  TEXT
-  #  assert_equal result, ls.exec
-  #end
+  def test_exec_with_all_options
+    ls = LsCommand.new(['a', 'l', 'r'], TEST_DIR)
+    result = <<~TEXT
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 k.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 j.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 i.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 h.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 g.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 f.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 e.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 d.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 c.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 b.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 a.txt
+      drwxr-xr-x  4 mashym91  staff  128  6 11 17:16 ..
+      drwxr-xr-x  13 mashym91  staff  416  6 11 22:59 .
+    TEXT
+    # ヒアドキュメントで最後に改行コードが入るので削除
+    assert_equal result.gsub(/\n$/, ''), ls.exec
+  end
 end

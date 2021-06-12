56861ea3b0fcc113944e36436be4b93aca473cf2
mashym91 <76610676+mashym91@users.noreply.github.com>
2021-06-12 10:05:02 +0900

    オプション2つを組み合わせたテストケース追加

--- 05.ls/test/ls_command_test.rb
+++ 05.ls/test/ls_command_test.rb
@@ -66,6 +66,58 @@ class LsCommandTest < Minitest::Test
     assert_equal result.gsub(/\n$/, ''), ls.exec
   end
 
+  def test_dir_al_options
+    ls = LsCommand.new(%w[a l], TEST_DIR)
+    result = <<~TEXT
+      drwxr-xr-x  13 mashym91  staff  416  6 11 22:59 .
+      drwxr-xr-x  4 mashym91  staff  128  6 11 17:16 ..
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 a.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 b.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 c.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 d.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 e.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 f.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 g.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 h.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 i.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 j.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 k.txt
+    TEXT
+    # ヒアドキュメントで最後に改行コードが入るので削除
+    assert_equal result.gsub(/\n$/, ''), ls.exec
+  end
+
+  def test_dir_ar_options
+    ls = LsCommand.new(%w[a r], TEST_DIR)
+    result = <<~TEXT
+      k.txt\tf.txt\ta.txt\t
+      j.txt\te.txt\t..\t
+      i.txt\td.txt\t.\t
+      h.txt\tc.txt\t\t
+      g.txt\tb.txt\t\t
+    TEXT
+    assert_equal result, ls.exec
+  end
+
+  def test_dir_lr_options
+    ls = LsCommand.new(%w[l r], TEST_DIR)
+    result = <<~TEXT
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 k.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 16:36 j.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 i.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 h.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 g.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 f.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 e.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 d.txt
+      -rw-r--r--  1 mashym91  staff  0  6 11 18:42 c.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 b.txt
+      drwxr-xr-x  3 mashym91  staff  96  6 11 18:43 a.txt
+    TEXT
+    # ヒアドキュメントで最後に改行コードが入るので削除
+    assert_equal result.gsub(/\n$/, ''), ls.exec
+  end
+
   def test_dir_all_options
     ls = LsCommand.new(%w[a l r], TEST_DIR)
     result = <<~TEXT
@@ -110,6 +162,21 @@ class LsCommandTest < Minitest::Test
 
   def test_file_all_options
     ls = LsCommand.new(%w[a l r], TEST_FILE)
+    assert_equal '-rw-r--r--  1 mashym91  staff  0  6 11 18:42 ./test/ls_test_dir/c.txt', ls.exec
+  end
+
+  def test_file_al_options
+    ls = LsCommand.new(%w[a l], TEST_FILE)
+    assert_equal '-rw-r--r--  1 mashym91  staff  0  6 11 18:42 ./test/ls_test_dir/c.txt', ls.exec
+  end
+
+  def test_file_ar_options
+    ls = LsCommand.new(%w[a r], TEST_FILE)
+    assert_equal "./test/ls_test_dir/c.txt\t\t\t\n", ls.exec
+  end
+
+  def test_file_lr_options
+    ls = LsCommand.new(%w[l r], TEST_FILE)
     assert_equal '-rw-r--r--  1 mashym91  staff  0  6 11 18:42 ./test/ls_test_dir/c.txt', ls.exec
   end
 end

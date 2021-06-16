326846f929bfed32e4a2e110344e8e8a923fae0c
mashym91 <mashym91@gmail.com>
2021-06-16 21:39:33 +0900

    わかりすいようにメソッド名を変更

--- 06.wc/lib/wc/command.rb
+++ 06.wc/lib/wc/command.rb
@@ -13,7 +13,7 @@ module WC
       if @files.empty?
         # 入力ファイル名を指定しない場合は標準入力から読み込む
         standard_input = $stdin.readlines
-        generate_display_format(standard_input.count, standard_input.join.split(/\s+/).count, standard_input.join.bytesize)
+        generate_one_line_for_display(standard_input.count, standard_input.join.split(/\s+/).count, standard_input.join.bytesize)
       else
         total_lines = 0
         total_words = 0
@@ -21,19 +21,19 @@ module WC
         files_display_info = []
         @files.each do |file|
           wc_file = WC::File.new(file)
-          files_display_info << generate_display_format(wc_file.lines, wc_file.words, wc_file.bytes, " #{file}")
+          files_display_info << generate_one_line_for_display(wc_file.lines, wc_file.words, wc_file.bytes, " #{file}")
           total_lines += wc_file.lines
           total_words += wc_file.words
           total_bytes += wc_file.bytes
         end
-        files_display_info << generate_display_format(total_lines, total_words, total_bytes, ' total') if @files.count > 1
+        files_display_info << generate_one_line_for_display(total_lines, total_words, total_bytes, ' total') if @files.count > 1
         files_display_info.join("\n")
       end
     end
 
     private
 
-    def generate_display_format(lines, words, bytes, suffix_str = nil)
+    def generate_one_line_for_display(lines, words, bytes, suffix_str = nil)
       if @options.include?('l')
         "#{convert_to_display_format(lines)}#{suffix_str}"
       else
